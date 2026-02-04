44b3b37c8d78d3bfd9101adffea1c23b7c115e7e
Gajanan Bidwai <gajananbidwai525@gmail.com>
2026-02-04 18:41:40 +0530

    public key pinning

--- SSLPinningDemo/SSLPinningDemo/NetworkManager.swift
+++ SSLPinningDemo/SSLPinningDemo/NetworkManager.swift
@@ -6,12 +6,29 @@
 //
 
 import Foundation
+import CommonCrypto
 
 class NetworkManager: NSObject {
     static let shared = NetworkManager()
+   let localPublicKey = "UByNN6wh6WJFNj04mBWbj+iAfJP3C60LXpHBZXmMXwk="
     
-    lazy var session: URLSession = { URLSession( configuration: .ephemeral, delegate: nil, delegateQueue: nil)}()
-
+    private let rsa2048Asn1Header:[UInt8] = [
+        0x30, 0x82, 0x01, 0x22, 0x30, 0x0d, 0x06, 0x09, 0x2a, 0x86, 0x48, 0x86,
+        0xf7, 0x0d, 0x01, 0x01, 0x01, 0x05, 0x00, 0x03, 0x82, 0x01, 0x0f, 0x00
+    ]
+    
+    private func sha256(data: Data) -> String {
+        var keyWithHeader = Data(rsa2048Asn1Header)
+        keyWithHeader.append(data)
+        var hash = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
+        keyWithHeader.withUnsafeBytes {
+            _ = CC_SHA256($0.baseAddress, CC_LONG(keyWithHeader.count), &hash)
+        }
+        return Data(hash).base64EncodedString()
+    }
+    
+    lazy var session: URLSession = { URLSession( configuration: .ephemeral, delegate: self, delegateQueue: nil)}()
+    
         private override init() {
             super.init()
         }
@@ -61,6 +78,28 @@ extension NetworkManager: URLSessionDelegate {
             completionHandler(.cancelAuthenticationChallenge, nil)
             return
         }
+        
+//        Public key pinning
+        if let serverPublicKey = SecCertificateCopyKey(certificate), let serverPublicKeyData = SecKeyCopyExternalRepresentation(serverPublicKey, nil) {
+            
+            let data: Data = serverPublicKeyData as Data
+            let serverHashKey = sha256(data: data)
+            
+//            Comparing server and local hash key
+            
+            if serverHashKey == localPublicKey {
+
+                print("Public key pinning is successfull")
+//                completionHandler(.useCredential, credential)
+            } else {
+                print("Public Key pinning is failed")
+                completionHandler(.cancelAuthenticationChallenge, nil)
+            }
+        }
+        
+        
+        
+        
         
         //SSL policy for domain check
         let policy = NSMutableArray()
